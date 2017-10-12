909b6237f9546c62e03d496c007f20e6ca1e9220
kPherox <admin@mail.kr-kp.com>
2017-10-12 18:01:48 +0900

    Add PuSH controller
    Add webhooks route map

---
+++ app/Http/Controllers/PushController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PushController extends Controller
+{
+    /**
+     * subscribe JMA Publish
+     *
+     * @return \Illuminate\Http\Response
+     */
+    function subscriber() {
+        //
+    }
+}
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,6 +37,8 @@ class RouteServiceProvider extends ServiceProvider
     {
         $this->mapApiRoutes();
 
+        $this->mapWebHooksRoutes();
+
         $this->mapWebRoutes();
 
         //
@@ -69,5 +71,20 @@ class RouteServiceProvider extends ServiceProvider
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
+    }
+ 
+    /**
+     * Define the "hooks" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapWebHooksRoutes()
+    {
+        Route::prefix('hooks')
+             ->middleware('api')
+             ->namespace($this->namespace)
+             ->group(base_path('routes/hooks.php'));
     }
 }
