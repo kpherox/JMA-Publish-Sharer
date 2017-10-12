b23675f4082806a4e9c8a43c2ef6d3f52ec8c4ef
kPherox <admin@mail.kr-kp.com>
2017-10-13 03:44:59 +0900

    Add guzzle error handling
    fix url string

--- app/Http/Controllers/PushController.php
+++ app/Http/Controllers/PushController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Log;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 
 class PushController extends Controller
 {
@@ -14,10 +16,11 @@ class PushController extends Controller
      */
     function subscriber(Request $request) {
         // Subscribe check
-        $hubMode = $request->input('hub_mode');
-        $hubChallenge = $request->input('hub_challenge');
+        $hubMode = $request->hub_mode;
+        $hubChallenge = $request->hub_challenge;
 
         if ($hubMode == 'subscribe' || $hubMode == 'unsubscribe') {
+            Log::debug($hubMode);
             return response($hubChallenge, 200)->header('Content-Type', 'text/plain');
         } else {
             Log::debug('hub_mode not subscribe|unsubscribe');
@@ -39,14 +42,19 @@ class PushController extends Controller
             return $message;
         }
         Log::debug('success feed parse');
-
-        $client = new GuzzleHttp\Client();
+        
+        $client = new Client();
         // Fetch JMA xml
         foreach ($feed->entry as $entry) {
-            $url = $entry->link['href'];
+            Log::debug($entry->title);
+            $url = (string)$entry->link['href'];
             Log::debug($url);
-            $response = $client->get($url);
-            Log::debug($res->getBody());
+            try {
+                $response = $client->get($url);
+            } catch (ClientException $e) {
+                Log::error($e->getMessage());
+                continue;
+            }
         }
     }
 }
