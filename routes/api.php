06803e0e82720abe658e891b9b0ff7c238132341
kPherox <admin@mail.kr-kp.com>
2017-10-12 17:06:25 +0900

    Versioning api

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+/**
+ * API version 1
+ */
+Route::prefix('v1')->group(function () {
+    Route::get('user', function (Request $request) {
+        return $request->user();
+    });
 });
+
