f868a2a7570647bafe40c085d24b4bbf37e450bd
kPherox <admin@mail.kr-kp.com>
2017-10-13 15:23:06 +0900

    Add certification hub_verify_token

--- app/Http/Controllers/PushController.php
+++ app/Http/Controllers/PushController.php
@@ -17,15 +17,23 @@ class PushController extends Controller
     function subscriber(Request $request) {
         // Subscribe check
         $hubMode = $request->hub_mode;
-        $hubChallenge = $request->hub_challenge;
-
-        if ($hubMode == 'subscribe' || $hubMode == 'unsubscribe') {
-            Log::debug($hubMode);
-            return response($hubChallenge, 200)->header('Content-Type', 'text/plain');
-        } else {
-            Log::debug('hub_mode not subscribe|unsubscribe');
+        if ($hubMode != 'subscribe' && $hubMode != 'unsubscribe') {
+            Log::error('hub_mode not subscribe|unsubscribe');
             return response('Not Found', 404)->header('Content-Type', 'text/plain');
         }
+
+        if (env('IS_HUB_VERIFY_TOKEN', false)) {
+            $hubVerifyToken = $request->hub_verify_token;
+            if ($hubVerifyToken != env('HUB_VERIFY_TOKEN')) {
+                Log::error('Incorrect hub_verify_token');
+                return response('Unknown Request', 404)->header('Content-Type', 'text/plain');
+            }
+        }
+        $hubChallenge = $request->hub_challenge;
+        Log::debug($hubMode);
+        Log::debug('Success subscribe check')
+
+        return response($hubChallenge, 200)->header('Content-Type', 'text/plain');
     }
 
     /**
@@ -36,6 +44,18 @@ class PushController extends Controller
     function receiveFeed(Request $request) {
         // Xml parse
         $content = $request->getContent();
+
+        if (env('IS_HUB_VERIFY_TOKEN', false)) {
+            if (!isset($request->header('X-Hub-Signature'))) {
+                return response('Invalid X-Hub-Signature', 404)->header('Content-Type', 'text/plain');
+            }
+            $signature = $request->header('X-Hub-Signature');
+            $hash = hash_hmac("sha1",$content,env('HUB_VERIFY_TOKEN');
+            if ($signature != $hash) {
+                return response('Invalid X-Hub-Signature', 404)->header('Content-Type', 'text/plain');
+            }
+
+        }
         if (false === ($feed = simplexml_load_string($content))) {
             $message = "feed Parse ERROR";
             Log::error($message);
