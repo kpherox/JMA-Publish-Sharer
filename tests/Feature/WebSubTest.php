e7be04600d9fe67461e3fcc4e6fadea60420544e
kPherox <admin@mail.kr-kp.com>
2018-05-10 01:30:29 +0900

    Add test for WebSub subscribe check all pattern

--- tests/Feature/WebSubTest.php
+++ tests/Feature/WebSubTest.php
@@ -11,20 +11,96 @@ class WebSubTest extends TestCase
 
     /**
      * Subscribe check test.
-     * unset 'X-Hub-Mode'
+     * Set 'hub.verify_token'
      *
      * @return void
      */
-    public function testSubscribeCheck()
+    public function testSubscribeCheckSuccess()
     {
-        $response = $this->get('/hooks/websub/subscriber');
+        $response = $this->call('GET', '/hooks/websub/subscriber', [
+                                    'hub_mode' => 'subscribe',
+                                    'hub_verify_token' => 'testwebsub'
+                                ]);
+
+        $response->assertStatus(200);
+    }
+
+    public function testSubscribeCheckOldPathSuccess()
+    {
+        $response = $this->call('GET', '/hooks/push/subscriber', [
+                                    'hub_mode' => 'subscribe',
+                                    'hub_verify_token' => 'testwebsub'
+                                ]);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Subscribe check test.
+     * Set an incorrect value to 'hub.verify_token'
+     *
+     * @return void
+     */
+    public function testSubscribeCheckIncorrectToken()
+    {
+        $response = $this->call('GET', '/hooks/websub/subscriber', [
+                                    'hub_mode' => 'subscribe',
+                                    'hub.verify_token' => 'testfail'
+                                ]);
+
+        $response->assertStatus(403);
+    }
+
+    public function testSubscribeCheckOldPathIncorrecetToken()
+    {
+        $response = $this->call('GET', '/hooks/push/subscriber', [
+                                    'hub_mode' => 'subscribe',
+                                    'hub.verify_token' => 'testfail'
+                                ]);
+
+        $response->assertStatus(403);
+    }
+
+    /**
+     * Subscribe check test.
+     * Does't set 'hub.verify_token'
+     *
+     * @return void
+     */
+    public function testSubscribeCheckNotExistToken()
+    {
+        $response = $this->call('GET', '/hooks/websub/subscriber', [
+                                    'hub_mode' => 'subscribe'
+                                ]);
+
+        $response->assertStatus(403);
+    }
+
+    public function testSubscribeCheckOldPathNotExistToken()
+    {
+        $response = $this->call('GET', '/hooks/push/subscriber', [
+                                    'hub_mode' => 'subscribe'
+                                ]);
+
+        $response->assertStatus(403);
+    }
+
+    /**
+     * Subscribe check test.
+     * Doesn't set 'hub.mode'
+     *
+     * @return void
+     */
+    public function testSubscribeCheckNotFound()
+    {
+        $response = $this->call('GET', '/hooks/websub/subscriber');
 
         $response->assertStatus(404);
     }
 
-    public function testSubscribeCheckOldPath()
+    public function testSubscribeCheckOldPathNotFound()
     {
-        $response = $this->get('/hooks/push/subscriber');
+        $response = $this->call('GET', '/hooks/push/subscriber');
 
         $response->assertStatus(404);
     }
