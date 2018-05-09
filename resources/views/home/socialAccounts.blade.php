6c6f8f1587bc44f2a5d70edcd465cac7ad99f877
kPherox <admin@mail.kr-kp.com>
2018-05-09 23:14:39 +0900

    Chnage .card.col-* -> .col-*>.card

--- resources/views/home/socialAccounts.blade.php
+++ resources/views/home/socialAccounts.blade.php
@@ -2,23 +2,25 @@
 @section ('title', 'Social Accounts')
 
 @section ('page-content')
-<div id="account-lists" class="card col-lg-5 col-xl-4">
-    <h5 class="card-header">Account Lists</h5>
-    <div class="card-body">
-        <h5>Twitter</h5>
-        <div class="list-group list-group-flush">
-            @if ($socialAccounts->where('provider_name', 'twitter')->exists())
-            @foreach ($socialAccounts->where('provider_name', 'twitter')->get() as $account)
-            <a class="list-group-item list-group-item-action" href="#twitter-{{ $account->account_name }}">
-                <img class="align-middle rounded-circle social-avatar" src="{{ $account->account_avatar }}" alt="Twitter Icon" />
-                &#64;{{ $account->account_name }}
-            </a>
-            @endforeach
-            @endif
-            <a class="list-group-item list-group-item-action" href="{{ route('twitter.linktouser') }}">
-                <button type="button" class="btn btn-secondary btn-lg btn-add p-0 rounded-circle">＋</button>
-                Link Account
-            </a>
+<div class="col-lg-5 col-xl-4">
+    <div id="account-lists" class="card">
+        <h5 class="card-header">Account Lists</h5>
+        <div class="card-body">
+            <h5>Twitter</h5>
+            <div class="list-group list-group-flush">
+                @if ($socialAccounts->where('provider_name', 'twitter')->exists())
+                @foreach ($socialAccounts->where('provider_name', 'twitter')->get() as $account)
+                <a class="list-group-item list-group-item-action" href="#twitter-{{ $account->account_name }}">
+                    <img class="align-middle rounded-circle social-avatar" src="{{ $account->account_avatar }}" alt="Twitter Icon" />
+                    &#64;{{ $account->account_name }}
+                </a>
+                @endforeach
+                @endif
+                <a class="list-group-item list-group-item-action" href="{{ route('twitter.linktouser') }}">
+                    <button type="button" class="btn btn-secondary btn-lg btn-add p-0 rounded-circle">＋</button>
+                    Link Account
+                </a>
+            </div>
         </div>
     </div>
 </div>
