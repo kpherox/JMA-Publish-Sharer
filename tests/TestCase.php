db967a604c524279f510072a04f745204b7cb228
kPherox <admin@mail.kr-kp.com>
2018-05-11 02:21:19 +0900

    Add db connection name for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,27 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public static $migrated = false;
+
+    public function setUp()
+    {
+        parent::setUp();
+        if (!self::$migrated) {
+            \Artisan::call('migrate:refresh');
+            self::$migrated = true;
+        }
+    }
+
+    public function tearDown()
+    {
+        foreach (\DB::select('SHOW TABLES') as $table) {
+            $columnName = 'Tables_in_'.\DB::connection('')->getDatabaseName();
+            $tableName = $table->$columnName;
+            if ($tableName === 'migrations') continue;
+            \DB::statement('TRUNCATE TABLE `' . $tableName . '`');
+        }
+        parent::tearDown();
+    }
+
 }
