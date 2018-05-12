c2b18496e143010eb120ceec692c2ca7d14e0243
kPherox <admin@mail.kr-kp.com>
2018-05-12 21:53:08 +0900

    Add xml for test
    Change test feed string to xml file

--- tests/Feature/WebSubTest.php
+++ tests/Feature/WebSubTest.php
@@ -33,8 +33,12 @@ class WebSubTest extends TestCase
                       ->with('http://*/*/b60694a6-d389-3194-a051-092ee9b2c474.xml')
                       ->andReturn(new Promise\FulfilledPromise(new Psr7\Response(200, [], '<?xml', 1.1)));
 
-        $atomFeed = '<?xml version="1.0" encoding="utf-8"?><feed xmlns="http://www.w3.org/2005/Atom" xml:lang="ja"><title>JMAXML publishing feed</title><subtitle>this feed is published by JMA</subtitle><updated>2012-08-15T07:01:01+09:00</updated><id>urn:uuid:be4342e2-ff73-363c-a3ed-66e05e977224</id><link rel="related" href="http://www.jma.go.jp/" /><link rel="self" href="http://xml.kishou.go.jp/*/*.xml" /><link rel="hub" href="http://alert-hub.appspot.com/" /><rights>Published by Japan Meteorological Agency</rights><entry><title>気象警報・注意報</title><id>urn:uuid:8e55b8d8-518b-3dc9-9156-7e87c001d7b5</id><updated>2012-08-15T07:00:00+09:00</updated><author><name>富山地方気象台</name></author><link href="http://*/*/8e55b8d8-518b-3dc9-9156-7e87c001d7b5.xml" type="application/xml" /><content type="text">【富山県気象警報・注意報】富山県では、強風、高波に注意してください。</content></entry><entry><title>気象警報・注意報</title><id>urn:uuid:b60694a6-d389-3194-a051-092ee9b2c474</id><updated>2012-08-15T07:00:00+09:00</updated><author><name>京都地方気象台</name></author><link href="http://*/*/b60694a6-d389-3194-a051-092ee9b2c474.xml" type="application/xml"/><content type="text">【京都府気象警報・注意報】京都府では、１５日昼過ぎから高波に注意して下さい</content></entry></feed>';
-        $hash = hash_hmac('sha1', $atomFeed, $this->verifyToken);
+        $atomFeed = fopen('tests/SampleData/jmaxml_atomfeed.xml', 'r');
+        fseek($atomFeed, 1);
+        $feedString = fread($atomFeed, 1540);
+        fclose($atomFeed);
+
+        $hash = hash_hmac('sha1', $feedString, $this->verifyToken);
 
         $headers = [
             'HTTP_user-agent' => 'AppEngine-Google; (+http://code.google.com/appengine; appid: s~alert-hub)',
@@ -42,7 +46,7 @@ class WebSubTest extends TestCase
             'HTTP_x-hub-signature' => 'sha1='.$hash
         ];
 
-        $response = $this->call('POST', self::$websubEndpoint, [], [], [], $headers, $atomFeed);
+        $response = $this->call('POST', self::$websubEndpoint, [], [], [], $headers, $feedString);
 
         $response->assertSuccessful();
     }
