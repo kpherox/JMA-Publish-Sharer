5e5eb84c95d92c48aa5c868482f9d84de78435bc
kPherox <admin@mail.kr-kp.com>
2018-05-25 18:36:28 +0900

    Add logout method to vue instance

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -18,5 +18,10 @@ window.Vue = require('vue');
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    methods: {
+        logout: () => {
+            document.getElementById('logout-form').submit();
+        }
+    }
 });
