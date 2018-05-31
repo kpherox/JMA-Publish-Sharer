cd48beda4a19febd1119faaafff1eecfcf4a3a90
kPherox <admin@mail.kr-kp.com>
2018-05-31 20:01:22 +0900

    Add description for button of the account settings

--- resources/assets/js/components/AccountSettings.vue
+++ resources/assets/js/components/AccountSettings.vue
@@ -10,29 +10,40 @@
             <div class="card border-light">
                 <h5 class="card-header bg-transparent border-danger">Danger Zone</h5>
 
-                <div class="card-body">
-                    <button class="btn btn-danger" type="button" data-toggle="modal" data-target="#unlinkModal">Unlink this account</button>
+                <div class="list-group list-group-flush">
+                    <div class="list-group-item d-sm-flex d-lg-block d-xl-flex">
+                        <p class="mb-0 p-2">
+                            This account unlink from you.<br/>
+                            However, you can't unlink this if it is the only linked account, if no email and password are set.
+                        </p>
+                        <button class="btn btn-danger ml-auto align-self-center" type="button" data-toggle="modal" data-target="#unlinkModal" :disabled="!isSafeUnlink">
+                            Unlink this account
+                        </button>
+                    </div>
+                </div>
 
-                    <!-- Unlink modal -->
-                    <div id="unlinkModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="unlinkModalTitle" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header border-danger">
-                                    <h5 class="modal-title" id="unlinkModalTitle">Unlink this account</h5>
+                <!-- Unlink modal -->
+                <div id="unlinkModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="unlinkModalTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header border-danger">
+                                <h5 class="modal-title" id="unlinkModalTitle">Unlink this account</h5>
 
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
 
-                                <div class="modal-body">
-                                    <p>This account will be deauthenticated. Are you okay?</p>
-                                </div>
+                            <div class="modal-body">
+                                <p class="mb-0">
+                                    This account (<strong>&#64;{{ nickname }} - {{ provider }}</strong>) will be deauthenticated.<br/>
+                                    Are you okay?
+                                </p>
+                            </div>
 
-                                <div class="modal-footer border-danger">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                                    <button type="button" class="btn btn-danger" :disabled="!isSafeUnlink" @click="unlinkAccount">OK</button>
-                                </div>
+                            <div class="modal-footer border-danger">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                <button type="button" class="btn btn-danger" @click="unlinkAccount">OK</button>
                             </div>
                         </div>
                     </div>
