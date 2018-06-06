51d5f55489e9860d58291e2976c67e054440f5a2
kPherox <admin@mail.kr-kp.com>
2018-06-06 15:43:21 +0900

    Replace macro groupConcat() to composer package

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\AbstractPaginator;
-use Illuminate\Database\Eloquent\Builder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,29 +16,6 @@ class AppServiceProvider extends ServiceProvider
     {
         // Add the following line
         \Schema::defaultStringLength(191);
-        Builder::macro('groupConcat', function(String $name, String $separator = null, Bool $distinct = false, String $columnName = null) {
-            $separator = $separator ? "'$separator'" : '\',\'';
-            $columnName = ' AS '.($columnName ?: $name);
-            $name = $distinct ? 'DISTINCT '.$name : $name;
-            $connection = config('database.default');
-            switch (config("database.connections.{$connection}.driver"))
-            {
-                case 'mysql':
-                case 'sqlite':
-                    $sql = 'GROUP_CONCAT('.$name.' SEPARATOR '.$separator.')';
-                    break;
-                case 'pgsql':
-                    $sql = 'ARRAY_TO_STRING(ARRAY(SELECT unnest(array_agg('.$name.'))), '.$separator.')';
-                    break;
-                case 'sqlsrv':
-                    $sql = '(SELECT '.$name.' + \',\' FOR XML PATH(\'\'))';
-                default:
-                    throw new \Exception('Driver not supported.');
-                    break;
-            }
-
-            return $this->selectRaw($sql . $columnName);
-        });
     }
 
     /**
