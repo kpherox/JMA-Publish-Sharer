7a7ab623a8ae0e038915dd1b552b8ac177b4d4ec
kPherox <admin@mail.kr-kp.com>
2018-06-09 21:46:22 +0900

    Change to draw isobar

--- resources/views/entry/tenkizu.blade.php
+++ resources/views/entry/tenkizu.blade.php
@@ -12,6 +12,34 @@
                     center: {lat: 36.59444, lng: 136.62556},
                     mapTypeId: 'terrain'
                 });
+                @json ($entry['Body']['MeteorologicalInfos']['MeteorologicalInfo']['Item']).forEach(item => {
+                    if (item.Kind.Property.Type === '\u7b49\u5727\u7dda') {
+                        drawPolyline(item.Kind.Property.IsobarPart.Line);
+                        return;
+                    }
+                });
+            }
+          , getPath = (vector) => {
+                return vector.split('/').filter(v => v).map(x => {
+                    let point = x.match(new RegExp('([()+-][0-9]+\.?[0-9]+?)','g'))
+                      , latlng = {
+                            lat: Number(point[0]),
+                            lng: Number(point[1])
+                        };
+
+                    return latlng;
+                });
+            }
+          , drawPolyline = (vector) => {
+                let polyline = getPath(vector)
+                  , path = new google.maps.Polyline({
+                        path: polyline,
+                        geodesic: true,
+                        strokeColor: '#000000',
+                        strokeOpacity: 0.5,
+                        strokeWeight: 1
+                    });
+                path.setMap(this.map);
             };
         </script>
         <script async defer src="https://maps.googleapis.com/maps/api/js?key={!! config('services.googleapi.maps.js.key') !!}&callback=initMap"></script>
